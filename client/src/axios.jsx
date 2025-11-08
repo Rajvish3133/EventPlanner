4e6e2d66e734c5c58d2d55b1166d2d43bdce3932
Raj Vishwakarma <raj.vishwakarma123@gmail.com>
2025-11-08 16:38:25 +0530

    render backend

--- client/src/axios.jsx
+++ client/src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api', // Make sure this matches your backend port
+  baseURL: 'https://eventplanner-backend-orj9.onrender.com/api', // Make sure this matches your backend port
   headers: {
     'Content-Type': 'application/json',
   },
